57d32fc97c8fa7745a6085b8c01113fd0499fdc5
_nguyen.du <1851061329@e.tlu.edu.vn>
2023-03-10 11:17:19 +0700

    Draw chart temp

--- scripts/chart.js
+++ scripts/chart.js
@@ -14,7 +14,6 @@ const read = async () => {
 };
 
 const connectAll = (db) => {
-    const ctx = document.getElementById('myChart');
     var dt = new Date();
     var month = dt.getMonth() + 1;
     var year = dt.getFullYear();
@@ -27,18 +26,19 @@ const connectAll = (db) => {
     }
 
     // Mực nước
+    const ctx = document.getElementById('myChart');
     const data = {
         labels: labels,
         datasets: [
             {
-                label: 'PCW Tank Hot Press',
+                label: 'Mực nước Tank Hot Press',
                 data: db.mực_nước.hotpress,
-                backgroundColor: ['rgba(255, 99, 132)'],
+                backgroundColor: 'rgba(255, 99, 132)',
             },
             {
-                label: 'PCW Tank Wet Line',
+                label: 'Mực nước Tank Wet Line',
                 data: db.mực_nước.wetline,
-                backgroundColor: ['rgba(255, 159, 64)'],
+                backgroundColor: 'rgba(255, 159, 64)',
             },
         ],
     };
@@ -51,24 +51,58 @@ const connectAll = (db) => {
     // Nhiệt độ nước
     const ctx1 = document.getElementById('myChart1');
     const data1 = {
-        labels: labels,
         datasets: [
             {
-                label: 'PCW Tank Hot Press',
-                data: db.nhiệt_độ.hotpress,
-                backgroundColor: ['rgb(255, 99, 132)'],
+                label: 'Nhiệt độ Tank Hot Press',
+                borderWidth: 2, // độ dày
+                borderColor: 'rgb(255, 99, 132)', // màu line
+                pointRadius: 0, // điển chấm data trên line
+                tension: 0.5, //độ uốn của line
+                backgroundColor: 'rgb(255, 99, 132)',
             },
             {
-                label: 'PCW Tank Wet Line',
-                data: db.nhiệt_độ.wetline,
-                backgroundColor: ['rgb(255, 159, 64)'],
+                label: 'Nhiệt độ Tank Wet Line',
+                borderWidth: 2, // độ dày
+                borderColor: 'rgb(255, 159, 64)', // màu line
+                pointRadius: 0, // điển chấm data trên line
+                tension: 0.5, //độ uốn của line,
+                backgroundColor: 'rgb(255, 159, 64)',
             },
         ],
     };
 
+    setInterval(() => {
+        data1.datasets[0].data.push({
+            x: Date.now(),
+            y: Object.values(db.nhiệt_độ.hotpress)[Object.values(db.nhiệt_độ.hotpress).length - 1],
+        });
+        data1.datasets[1].data.push({
+            x: Date.now(),
+            y: Object.values(db.nhiệt_độ.wetline)[Object.values(db.nhiệt_độ.wetline).length - 1],
+        });
+    }, 1000);
+
     new Chart(ctx1, {
-        type: 'bar',
+        type: 'line',
         data: data1,
+        options: {
+            scales: {
+                x: {
+                    type: 'realtime',
+                    grid: {
+                        display: true, //kẻ dóng hàng dọc
+                    },
+                    realtime: {
+                        duration: 1 * 60 * 1000, //hiển thị 2 phut data
+                        delay: 1000, // lùi data hiển thị về sau 1s
+                    },
+                },
+                y: {
+                    beginAtZero: true,
+                    type: 'linear',
+                },
+            },
+        },
     });
 };
 
@@ -116,6 +150,18 @@ const check = (db) => {
         }
     }
     if (db) {
-        document.getElementById('left-wapper').getSVGDocument().getElementById('style1190').append('@import url(../css/styleSvg.css)');
+        document
+            .getElementById('left-wapper')
+            .getSVGDocument()
+            .getElementById('style1190')
+            .append('@import url(../css/styleSvg.css)');
+        document.getElementById('left-wapper').getSVGDocument().getElementById('path801').style.fill = '#00ff37';
+        document.getElementById('left-wapper').getSVGDocument().getElementById('path803').style.fill = '#00ff37';
+        document.getElementById('left-wapper').getSVGDocument().getElementById('path801').style.animation =
+            'fanRotate 1s linear infinite';
+        document.getElementById('left-wapper').getSVGDocument().getElementById('path801').style.transformOrigin =
+            'center';
+        document.getElementById('left-wapper').getSVGDocument().getElementById('path801').style.transformBox =
+            'fill-box';
     }
 };
